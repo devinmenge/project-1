63b1875a93773416d27d887a213c246681be26b2
Devin Menge <devinmenge@gmail.com>
2024-12-18 19:47:34 -0500

    added replacing td contents

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,18 @@
 // to do: add consts, functions, listener, etc.
 
+function getInputValue(button) {
+    // Get the input element by its name attribute
+    // var inputField = document.getElementsByName('person_0.fname')[0];
+    var tdElement = button.closest('td');
+    
+    // Find the input field inside this <td>
+    var inputField = tdElement.querySelector('input[name="task"]');
+
+    var inputValue = inputField.value;  // Get the value entered in the input field
+    
+    // Get the <td> element that contains the input field and buttons
+    var tdElement = inputField.closest('td');
+    
+    // Set the text inside the <td> to the input value
+    tdElement.innerHTML = inputValue;  // Insert the input value inside the <td>
+  }
